963bed7811495efc049bb7e859aaf139c42dec93
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-18 16:31:02 +0300

    feat: completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,28 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+// import * as RMFunctions from "./parks/RockyMountain";
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(trees);
+  wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+//   console.log(RMFunctions.trees);
+// RMFunctions.wildlife();
+// RMFunctions.elevation();
+
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
